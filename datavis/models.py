3dee63720ffd10c7eab39e95aae35fc0795c0ce2
rodcruzh <rodrigo.cruz.hz@gmail.com>
2017-03-29 22:53:02 -0500

    Modify dataset model - Results form

--- datavis/models.py
+++ datavis/models.py
@@ -22,6 +22,8 @@ class DataSet(models.Model):
     open_data_source = models.ForeignKey(OpenDataSource, on_delete=models.CASCADE)
     category_dataset = models.ForeignKey(CategoryDataSet, on_delete=models.CASCADE)
     identifier = models.CharField(max_length=200)
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
 
     def __str__(self):
         return self.identifier
