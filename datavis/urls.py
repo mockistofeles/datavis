e649a32f3af74846bfd592d82d0fff8f33aa4102
rodcruzh <rodrigo.cruz.hz@gmail.com>
2017-07-25 22:35:05 -0500

    Amend view methods to generic class view

--- datavis/urls.py
+++ datavis/urls.py
@@ -8,8 +8,8 @@ app_name = 'datavis'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
-    url(r'^search/', views.search, name='search'),
+    url(r'^search/', views.SearchDatasetList.as_view(), name='search'),
     url(r'^filter/', views.filter, name='filter'),
-    url(r'^detail/(?P<dataset_id>[0-9]+)/', views.detail, name='detail'),
+    url(r'^detail/(?P<pk>[0-9]+)/', views.SearchDatasetDetail.as_view(), name='detail'),
     url(r'^datavis/(?P<dataset_id>[0-9]+)/', views.datavis, name='datavis')
 ]
--- datavis/views.py
+++ datavis/views.py
@@ -1,6 +1,7 @@
 from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, DetailView
 from sodapy import Socrata
 from .models import OpenDataSource, CategoryDataSet, DataSet
 import json
@@ -9,24 +10,6 @@ import json
 def index(request):
     template = loader.get_template('datavis/index.html')
     context = {'title': 'Home'}
-    return HttpResponse(template.render(context, request))
-
-
-def search(request):
-    topic = request.POST['topic']
-    datasets = []
-    open_data_sources = []
-    qs = DataSet.objects.filter(category_dataset__name__icontains=topic)
-    for q in qs:
-        datasets.append(q)
-        if open_data_sources.count(q.open_data_source) == 0:
-            open_data_sources.append(q.open_data_source)
-    template = loader.get_template('datavis/ds-list.html')
-    context = {
-        'topic': topic,
-        'datasets': datasets,
-        'open_data_sources': open_data_sources
-    }
     return HttpResponse(template.render(context, request))
 
 
@@ -53,11 +36,6 @@ def filter(request):
     )
 
 
-def detail(request, dataset_id):
-    dataset = get_object_or_404(DataSet, pk=dataset_id)
-    return render(request, 'datavis/ds-detail.html', {'dataset': dataset})
-
-
 def datavis(request, dataset_id):
     ods = OpenDataSource.objects.get(pk=dataset_id)
     client = Socrata(ods.website, ods.token, ods.user, ods.password)
@@ -69,3 +47,32 @@ def datavis(request, dataset_id):
     data = json.dumps(data, indent=4, sort_keys=True)
     context = {'data': data, 'metadata': metadata, 'dataset': dataset}
     return HttpResponse(template.render(context, request))
+
+
+class SearchDatasetList(ListView):
+    template_name = 'datavis/ds-list.html'
+    context_object_name = 'datasets'
+    open_data_sources = []
+
+    def get_queryset(self):
+        qs = DataSet.objects.filter(category_dataset__name__icontains=self.request.GET['topic'])
+        for q in qs:
+            if self.open_data_sources.count(q.open_data_source) == 0:
+                self.open_data_sources.append(q.open_data_source)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchDatasetList, self).get_context_data(**kwargs)
+        context['open_data_sources'] = self.open_data_sources
+        context['topic'] = self.request.GET['topic']
+        return context
+
+
+class SearchDatasetDetail(DetailView):
+    model = DataSet
+    template_name = 'datavis/ds-detail.html'
+    context_object_name = 'dataset'
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchDatasetDetail, self).get_context_data(**kwargs)
+        return context
